abd3d7d2359ac915be457d2876aa67cb7bf3362e
Chrys <howlhund@gmail.com>
2026-02-10 09:50:02 -0700

    Forgot negative sign
    Fix subtract function to return correct result.

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ def test_add():
     assert add('space', 'ship') == 'spaceship'
 
 def subtract(a,b):
-    return a - b
+    return a - b # this used to be + to make the test fail. so the large update is just changing it to -
 
 # uncomment the following test in step 5 <- you need this part so the test actually works
 def test_subtract():
-    assert subtract(2, 3) == 1
+    assert subtract(2, 3) == -1
